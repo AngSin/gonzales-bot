74ce3c98322fc5dc47c58b5ea45fcc24c7b3f863
angadsingh <angad17@hotmail.com>
2024-12-21 21:30:42 +0000

    create unique tag for each docker image

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 # Variables
 REPOSITORY_NAME="gonzalez-bot"
 ECR_URI="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
+TAG=$(date +%Y-%m-%d|%H:%M:%S) # Unique tag for each build
 
 # Check if the repository exists; if not, create it
 REPO_CHECK=$(aws ecr describe-repositories --repository-names $REPOSITORY_NAME --region $AWS_REGION 2>/dev/null)
@@ -14,18 +15,18 @@ else
 fi
 
 echo "Building Docker image..."
-docker buildx build --platform linux/amd64,linux/arm64 -t $REPOSITORY_NAME --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t $REPOSITORY_NAME:$TAG --push .
 
 echo "Authenticating Docker to ECR..."
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_URI
 
 echo "Tagging Docker image..."
-docker tag $REPOSITORY_NAME:latest $ECR_URI/$REPOSITORY_NAME:latest
+docker tag $REPOSITORY_NAME:$TAG $ECR_URI/$REPOSITORY_NAME:$TAG
 
 echo "Pushing Docker image to ECR..."
-docker push $ECR_URI/$REPOSITORY_NAME:latest
+docker push $ECR_URI/$REPOSITORY_NAME:$TAG
 
-IMAGE_URI="$ECR_URI/$REPOSITORY_NAME:latest"
+IMAGE_URI="$ECR_URI/$REPOSITORY_NAME:$TAG"
 
 echo "Image successfully pushed to ECR: $IMAGE_URI"
 
