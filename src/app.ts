03cadd98b892bed55f9cdb8cf00d11807b63706d
angadsingh <angad17@hotmail.com>
2024-12-23 16:51:56 +0000

    Move code to handleMessage

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,15 @@
-import {Bot, Context} from "grammy";
+import {Bot} from "grammy";
 import {getMandatoryEnvVariable} from "./utils/getMandatoryEnvVariable";
-import {getAddressFromMessage, getTickerFromMessage} from "./utils/string";
-import {searchAndRetrievePair} from "./services/api";
-import {MessagingService} from "./services/MessagingService";
-import {SolanaKeyService} from "./services/SolanaKeyService";
-import {Logger} from "@aws-lambda-powertools/logger";
 import handleStart from "./handlers/handleStart";
 import handleBuy from "./handlers/handleBuy";
+import handleMessage from "./handlers/handleMessage";
 
 const botToken = getMandatoryEnvVariable("TELEGRAM_BOT_TOKEN");
 
-const logger = new Logger({ serviceName: "App" });
 const bot = new Bot(botToken);
-const messagingService = new MessagingService();
-const solanaKeyService = new SolanaKeyService();
-
 bot.command("start", handleStart);
 
-bot.on("message", async (context: Context) => {
-    const { message } = context;
-    console.log(`Received message: ${JSON.stringify(message)}\n`);
-    if (!message) return; // nothing to do here
-    const ticker = getTickerFromMessage(message);
-    const address = getAddressFromMessage(message);
-    const addressOrTicker = address || ticker;
-    if (!addressOrTicker) return;
-
-    // message verification complete
-    console.log(`Found address or ticker: ${addressOrTicker}`);
-    const pair = await searchAndRetrievePair(addressOrTicker);
-    console.log(`Found token pair: ${JSON.stringify(pair)}\n`);
-
-    await messagingService.replyWithPairInfo(context, pair);
-});
+bot.on("message", handleMessage);
 
 bot.on("callback_query:data", async (context) => {
     const callbackData = context.callbackQuery?.data;
---
+++ src/handlers/handleMessage.ts
@@ -0,0 +1,25 @@
+import {Context} from "grammy";
+import {MessagingService} from "../services/MessagingService";
+import DexscreenerService from "../services/api";
+import {getAddressFromMessage, getTickerFromMessage} from "../utils/string";
+
+const handleMessage = async (context: Context) => {
+    const messagingService = new MessagingService();
+    const dexscreenerService = new DexscreenerService();
+    const {message} = context;
+    console.log(`Received message: ${JSON.stringify(message)}\n`);
+    if (!message) return; // nothing to do here
+    const ticker = getTickerFromMessage(message);
+    const address = getAddressFromMessage(message);
+    const addressOrTicker = address || ticker;
+    if (!addressOrTicker) return;
+
+    // message verification complete
+    console.log(`Found address or ticker: ${addressOrTicker}`);
+    const pair = await dexscreenerService.searchAndRetrievePair(addressOrTicker);
+    console.log(`Found token pair: ${JSON.stringify(pair)}\n`);
+
+    await messagingService.replyWithPairInfo(context, pair);
+}
+
+export default handleMessage;
