efa07d9f10a4cbda7ac39a323e61e9109b51571a
angadsingh <angad17@hotmail.com>
2024-12-26 22:11:48 +0000

    Trial: add logging in handleDecimals

--- src/handlers/handleDecimals.ts
+++ src/handlers/handleDecimals.ts
@@ -1,8 +1,12 @@
 import SolanaService from "../services/SolanaService";
 import {Context} from "grammy";
 import {MessagingService} from "../services/MessagingService";
+import {Logger} from "@aws-lambda-powertools/logger";
+
+const logger = new Logger({ serviceName: 'handleDecimals' })
 
 const handleDecimals = async (context: Context) => {
+    logger.info(`handling decimals call`, { context });
     const solanaService = new SolanaService();
     const messagingService = new MessagingService();
     const humanFriendlyBalance = await solanaService.getHumanFriendlyTokenBalance('21AErpiB8uSb94oQKRcwuHqyHF93njAxBSbdUrpupump', '123456789')
