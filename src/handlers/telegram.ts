323ebd657090f756e9cdc992f20d8222f72266f4
angadsingh <angad17@hotmail.com>
2024-12-27 15:50:48 +0000

    Add handleExport to telegram.ts

--- src/handlers/telegram.ts
+++ src/handlers/telegram.ts
@@ -3,27 +3,27 @@ import { APIGatewayProxyEventV2 } from 'aws-lambda';
 import {Context} from "grammy";
 import handleStart from "./handleStart";
 import handleDecimals from "./handleDecimals";
+import {Commands} from "./types";
+import handleExport from "./handleExport";
 
 const logger = new Logger({ serviceName: 'TelegramHandler' });
 
 export const handler = async (event: APIGatewayProxyEventV2) => {
     logger.info(`Received telegram event`, { event });
     const context = JSON.parse(event.body || '{}') as Context;
-    if (!context.message) {
-        logger.error(`Error, malformed request`);
+    if (!context.callbackQuery && !context.message) {
+        logger.error(`Error, missing message & data`, { context });
         return;
     }
     logger.info(`Received context`, { context });
-    const text = context.message?.text;
-    switch (text) {
-        case '/start':
-            await handleStart(context);
-            return;
-        case '/decimals':
-            await handleDecimals(context);
-            return;
+    const command = context.callbackQuery?.data || context.message?.text;
+    switch (command) {
+        case Commands.START:
+            return await handleStart(context);
+        case Commands.EXPORT:
+            return await handleExport(context);
         default:
-            logger.error(`Unhandled message text: ${text}`);
+            logger.error(`Unhandled message text: ${command}`);
             return;
     }
 };
